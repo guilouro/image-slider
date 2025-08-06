317ff7480bade37bdaefa7372bc26beeb8a314e3
Guilherme Louro <guilherme-louro@hotmail.com>
2025-08-06 11:21:52 -0300

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Image Slider</h1>
+      <p>
+        This is a draggable image slider. Click and drag to navigate between
+        images.
+      </p>
+    </>
+  );
+}
+
+export default App;
